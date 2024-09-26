7b9302e075e0031b37a12e7851da36fc5831872a
Ch Ammar <124226871+iammar99@users.noreply.github.com>
2024-09-26 10:02:28 +0500

    Time: 144 ms (90.78%) | Memory: 65.1 MB (68.76%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -2,30 +2,30 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function (nums) {
-    let res = []
-
-    nums.sort((a, b) => a - b)
-
-
-
-    for (let i = 0; i < nums.length; i++) {
-
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
-        let left_pointer = i + 1, right_pointer = nums.length - 1;
-
-        while (left_pointer < right_pointer) {
-            if (nums[left_pointer] + nums[right_pointer] + nums[i] === 0) {
-
-                res.push([nums[left_pointer], nums[right_pointer], nums[i]])
-                while (nums[left_pointer] === nums[left_pointer + 1]) left_pointer++;
-                while (nums[right_pointer] === nums[right_pointer - 1]) right_pointer--;
-
+var threeSum = function(nums) {
+        let res = []
+    nums.sort((a,b)=>a-b)
+    for(let i = 0 ; i < nums.length ; i++){
+        if(i>0 && nums[i]==nums[i-1]){
+            continue
+        }
+        let left_pointer = i + 1
+        let right_pointer = nums.length - 1
+        while(left_pointer < right_pointer){
+            let sum = nums[i] + nums[left_pointer] + nums[right_pointer]
+            if(sum == 0){
+                res.push([nums[i] , nums[left_pointer] , nums[right_pointer]])
+            while(nums[left_pointer + 1] == nums[left_pointer]){
+                left_pointer++
             }
-            if (nums[left_pointer] + nums[right_pointer] + nums[i] > 0) {
+            while(nums[right_pointer] == nums[right_pointer - 1]){
                 right_pointer--
             }
-            else {
+            }
+            if(sum > 0){
+                right_pointer--
+            }
+            else{
                 left_pointer++
             }
         }
