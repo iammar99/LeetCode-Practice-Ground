2d93ad9cc415b364753ad7ab3a3723f66716c514
Ch Ammar <124226871+iammar99@users.noreply.github.com>
2024-10-12 22:41:34 +0500

    Time: 65 ms (50.13%) | Memory: 51.4 MB (56.55%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var clearDigits = function (s) {
+    let stack = []
+    for (let i = 0; i < s.length; i++) {
+        stack.push(s[i])
+        let str = stack[stack.length - 1]
+        if (s.charCodeAt(i) <= 57 && s.charCodeAt(i) >= 48) {
+            stack.pop()
+            stack.pop()
+        }
+    }
+    return stack.join("")
+};
