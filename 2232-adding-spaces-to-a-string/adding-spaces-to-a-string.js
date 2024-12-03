40d490cc7c04df26576fceea70bf5d426b9ef674
Ch Ammar <124226871+iammar99@users.noreply.github.com>
2024-12-03 16:37:45 +0500

    Time: 122 ms (41.82%) | Memory: 89.5 MB (54.84%) - LeetSync

---
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {number[]} spaces
+ * @return {string}
+ */
+var addSpaces = function (s, spaces) {
+    let res = ""
+    let pointer = 0
+    for (let i = 0; i < s.length; i++) {
+        if (i == spaces[pointer]) {
+            res += " "
+            pointer++
+        }
+        res += s[i]
+    }
+    return res
+};
