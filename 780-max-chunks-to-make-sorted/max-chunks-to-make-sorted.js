60a3b2fa980a40983dee991c796f72d6824690c8
Ch Ammar <124226871+iammar99@users.noreply.github.com>
2024-12-20 00:46:06 +0500

    Time: 0 ms (100.00%) | Memory: 48.9 MB (36.45%) - LeetSync

---
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maxChunksToSorted = function(arr) {
+    let runningSum = 0, expectedSum = 0, chunks = 0;
+        for (let i = 0; i < arr.length; i++) {
+            runningSum += arr[i];
+            expectedSum += i;
+            if (runningSum === expectedSum) {
+                chunks++;
+            }
+        }
+        return chunks;
+};
