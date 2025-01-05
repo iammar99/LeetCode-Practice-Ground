340818de1bf9ed8869711ac91ae27c0146a41b35
Ch Ammar <124226871+iammar99@users.noreply.github.com>
2025-01-05 22:43:38 +0500

    Time: 44 ms (46.67%) | Memory: 72.5 MB (86.67%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {number[][]} shifts
+ * @return {string}
+ */
+var shiftingLetters = function (s, shifts) {
+    const alphabets = Array.from({ length: 26 }, (_, i) => String.fromCharCode(97 + i));
+    let arr = s.split("")
+    let count = new Array(s.length + 1).fill(0)
+    for (const [start, end, direction] of shifts) {
+        const value = direction === 1 ? 1 : -1;
+        count[start] += value;
+        count[end + 1] -= value;
+    }
+    for (let i = 1; i < s.length; i++) {
+        count[i] += count[i - 1];
+    }
+    for (let i = 0; i < arr.length; i++) {
+        const oldIndex = alphabets.indexOf(arr[i]);
+        if (oldIndex === -1) continue;
+        let index = (count[i] + oldIndex) % 26;
+        if (index < 0) {
+            index += 26;
+        }
+        arr[i] = alphabets[index];
+    }
+    return arr.join("")
+};
