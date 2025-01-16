85b6c08d5a54a9cba1440cad424e7ad43b2b4898
Ch Ammar <124226871+iammar99@users.noreply.github.com>
2025-01-16 10:36:15 +0500

    Time: 1 ms (91.27%) | Memory: 57.1 MB (29.75%) - LeetSync

---
+++ 338-counting-bits/counting-bits.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function (n) {
+    let res = new Array(n+1).fill(0)
+    for(let i = 0 ; i <= n ; i++){
+        res[i] = res[i>>1]+(i&1)
+    }
+    return res
+};
