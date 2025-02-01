9350320ddaee21ac5fd5a4264770c1d4bd4be003
Ch Ammar <124226871+iammar99@users.noreply.github.com>
2025-02-01 22:26:41 +0500

    Time: 0 ms (100.00%) | Memory: 51.4 MB (39.24%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.js
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.js
@@ -2,17 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var countPartitions = function(nums) {
-   let total = nums.reduce((acc, curr) => acc + curr, 0);
-    let left = 0;
-    let count = 0;
-    for (let i = 0; i < nums.length - 1; i++) { 
-        left += nums[i];
-        let right = total - left;
-         if ((left + right) % 2 === 0) {
-            count++;
+var countPartitions = function (nums) {
+    let right = 0
+    let left = 0
+    let ans = 0
+    for (let i = 0; i < nums.length; i++) {
+        right += nums[i]
+    }
+    for (let i = 1; i < nums.length; i++) {
+        left += nums[i]
+        right -= nums[i]
+        if (Math.abs(right - left) % 2 == 0) {
+            ans++
         }
     }
-
-    return count;
+    return ans
 };
