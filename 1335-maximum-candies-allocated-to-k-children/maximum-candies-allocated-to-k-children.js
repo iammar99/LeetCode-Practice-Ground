d391e281416620de3d2c1b4dfdb35fef4cfeae26
Ch Ammar <124226871+iammar99@users.noreply.github.com>
2025-03-15 21:57:59 +0500

    Time: 121 ms (6.25%) | Memory: 71.2 MB (9.38%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} candies
+ * @param {number} k
+ * @return {number}
+ */
+var maximumCandies = function (candies, k) {
+    let left = 1, right = Math.max(...candies);
+    let result = 0;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let childrenCount = candies.reduce((sum, pile) => sum + Math.floor(pile / mid), 0);
+
+        if (childrenCount >= k) {
+            result = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return result;
+};
