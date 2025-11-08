54da34b89209a3d59c68a49cf3cd56e1145f3405
Ch Ammar <124226871+iammar99@users.noreply.github.com>
2025-11-08 22:06:40 +0500

    Time: 1 ms (97.50%) | Memory: 61.8 MB (13.33%) - LeetSync

---
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function (root) {
+    if (!root) return [];
+
+    let result = [];
+    let queue = [root];
+
+    while (queue.length > 0) {
+        let levelSize = queue.length;
+        let currentLevel = [];
+
+        for (let i = 0; i < levelSize; i++) {
+            let node = queue.shift();
+            currentLevel.push(node.val);
+
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        let sum = 0
+        for (let i = 0; i < currentLevel.length; i++) {
+            sum += currentLevel[i]
+        }
+        result.push(sum / currentLevel.length)
+
+    }
+
+    return result;
+};
